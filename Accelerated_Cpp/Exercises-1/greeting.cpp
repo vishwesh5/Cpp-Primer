eba09c5fc5d4020ada8f5805d470c7a3bc0fb14d
vishwesh5 <vishweshshrimali5@gmail.com>
2019-04-05 09:12:31 +0530

    Add Exercises-1

---
+++ Accelerated_Cpp/Exercises-1/greeting.cpp
@@ -0,0 +1,36 @@
+/*
+
+	Please enter your first name: Estragon
+
+	********************
+	*                  *
+	* Hello, Estragon! *
+	*                  *
+	********************
+
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Please enter your name: ";
+  std::string name;
+  std::cin >> name;
+
+  const std::string greeting = "Hello, " + name + "!";
+
+  const std::string spaces(greeting.size(), ' ');
+  const std::string second = "* " + spaces + " *";
+  const std::string first(second.size(), '*');
+
+  std::cout << std::endl;
+  std::cout << first << std::endl;
+  std::cout << second << std::endl;
+  std::cout << "* " << greeting << " *" << std::endl;
+  std::cout << second << std::endl;
+  std::cout << first << std::endl;
+
+  return 0;
+}
---
+++ Chapter-02/multiLineString.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  std::cout << "a really, really long string literal "
+	    << "that spans two lines" << std::endl;
+  return 0;
+}
